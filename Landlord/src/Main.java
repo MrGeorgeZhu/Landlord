4fe07913c81014a099afb26c31e6bad5e5b66d52
GeorgeZhu <GeorgeZhu@10.0.0.9>
2017-02-15 19:53:44 -0700

    working

--- Landlord/src/Main.java
+++ Landlord/src/Main.java
@@ -7,6 +7,7 @@ public class Main
 		static ArrayList <Card> player2 = new ArrayList <Card>();
 		static ArrayList <Card> player3 = new ArrayList <Card>();
 		static ArrayList <Card> a = new ArrayList <Card>();
+		static ArrayList <Card> b = new ArrayList <Card>();
 		public static void main(String[] args)
 			{
 				SetDeck.generateDeck();
@@ -39,6 +40,8 @@ public class Main
 		    System.out.println(phrase + ", what is your name?");
 		    name = userInput.nextLine();
 		    System.out.println("Hello, " + name + "!");
+		    delay();
+		    System.out.println();
 		}
 		
 		public static void deal(){
@@ -68,24 +71,43 @@ public class Main
 				int counter = 1;
 				System.out.println("Here are the cards you have rigth now:");
 				for (Card c: player1){
-					System.out.println(counter + ". " + c.getFace() + " of " + c.getSuit());						
+					if(c.getRank()==14 || c.getRank()==15){
+						System.out.println(counter + ". " + c.getFace());
+					} else {
+						System.out.println(counter + ". " + c.getFace() + " of " + c.getSuit());
+					}											
 					counter++;
 				}
-				System.out.println("Type in the number(s) of the card you want to put down. Seperate them by \",\".");
+				System.out.println("Type in the number(s) of the card you want to put down. Seperate them by \",\". Type \"pass\" to skip this round.");
 				Scanner userInput = new Scanner(System.in);
 				String temp = userInput.nextLine();
-				String[] b = temp.split(",");
-				for(int i = 0; i < b.length; i++){
-					if(Integer.parseInt(b[i])>=1 && Integer.parseInt(b[i])<=player1.size()){}
-					else{System.out.println("Illegal input!"); play();}
-				}
-				for(int i = 0; i < b.length; i++){
-					a.add(player1.get(Integer.parseInt(b[i])-1));
-				}
-				for(int i = b.length-1; i>=0; i--){
-					player1.remove(Integer.parseInt(b[i])-1);
-				}
+				
+				if(temp.equalsIgnoreCase("pass")){} else {
+					String[] input = temp.split(",");
+					
+					for(int i = 0; i < input.length; i++){
+						if(Integer.parseInt(input[i])>=1 && Integer.parseInt(input[i])<=player1.size()){} //Need to have a solution for letters input
+						else{System.out.println("Illegal input!"); play();}
+					}//Check for illegal input
+					
+					if(a.size()==0){
+						for(int i = 0; i < input.length; i++){
+							a.add(player1.get(Integer.parseInt(input[i])-1));
+						} //Pass selected cards to arraylist a
+					} else {
+						for(int i = 0; i < input.length; i++){
+							b.add(player1.get(Integer.parseInt(input[i])-1));
+						} //Pass selected cards to arraylist b
+						
+					}
+					
+					
+					for(int i = input.length-1; i>=0; i--){
+						player1.remove(Integer.parseInt(input[i])-1);
+					}//remove selected cards from player arraylist
+				}				
 				delay();
+				System.out.println();
 			}
 		}
 		
--- Landlord/src/Rules.java
+++ Landlord/src/Rules.java
@@ -1,11 +1,25 @@
 
 public class Rules
 	{
-		public static void o(){
-			if (Main.a.size()==1){
-				
-			} else if (Main.a.size()==2){
-				
+		public static void o(){				
+				if (Main.a.size()==1){if(Main.b.get(0).getRank()>Main.a.get(0).getRank()){Main.a.remove(0); Main.a.add(Main.b.get(0)); Main.b.remove(0);}else{System.out.println("Illegal!"); Main.play();}}
+				if (Main.a.size()==2){
+					if(Main.a.get(0).getFace().equals(Main.a.get(0).getFace())){
+						
+				    } else if ((Main.a.get(0).getRank() == 14 || Main.a.get(0).getRank() == 15) && (Main.a.get(1).getRank() == 14 || Main.a.get(1).getRank() == 15)){
+				    	System.out.println("Wow! The biggest bomb in game, you can lead the new round.");
+				    	for(int i = 0; i < Main.a.size(); i++){
+				    		Main.a.remove(i);
+				    	}
+				    	for(int i = 0; i < Main.b.size(); i++){
+				    		Main.b.remove(i);
+				    	}
+				    } else {
+				    	System.out.println("Illegal!"); 
+				    	Main.play();
+				    }
+				}
 			}
-		}
+			
 	}
+	
